e5a3a2ae78b807fc6b0f2e07ec988cc3888ad36f
Alexandra Lupoi <alupoi18@gmail.com>
2021-12-02 22:39:50 +0200

    get calendar holidays from api

---
+++ lib/src/data/calendar_api.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class CalendarApi {
+  Future<List<dynamic>> getHolidays() async {
+    final Uri uri = Uri(
+      scheme: 'https',
+      host: 'calendarific.com',
+      pathSegments: <String>['api', 'v2', 'holidays'],
+      queryParameters: <String, String>{
+        'api_key': '84109344d1c9cf05e68ad735a08628d8673eb12c',
+        'country': 'RO',
+        'year': '2021'
+      }
+    );
+
+    final Response response = await get(uri);
+
+    if(response.statusCode != 200) {
+      throw StateError('Error fetching the holidays');
+    }
+
+    final Map<String, dynamic> body = jsonDecode(response.body) as Map<String, dynamic>;
+    final Map<String, dynamic> data = body['response'] as Map<String, dynamic>;
+    final List<dynamic> holidays = data['holidays'] as List<dynamic>;
+
+    return holidays;
+  }
+}
